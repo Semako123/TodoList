0a5e7ccca5f4469a2754d0df8aa021daa4af53f9
semako123 <zosusemenoch@gmail.com>
2022-09-17 00:55:48 +0100

    Set up the application Add application resources Create react components

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # TodoList
+
 This is a basic todo list app built with react, flask(Python) and MySQL database
+
+Features:
+
+> Add notes
+> Delete notes
+> Update notes
+> Sign up/ Login
+> Notes time-stamps
 
 Design:
 Font: Livvic
--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import Footer from './components/footer/footer';
+import Navbar from './components/navbar/navbar';
+import Notes from './components/notes/notes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Notes />
+      <Footer /> 
+    </>
   );
 }
 
